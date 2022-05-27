1ce5187157a097bda8d0babb9cbbaedcac8feec2
unknown <alex.178.ko@gmail.com>
2022-05-27 22:23:57 +0800

    Completed File metadata project

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+let bodyParser = require('body-parser')
 
 var app = express();
 
+//Set up multer
+const multer = require("multer");
+const upload = multer({ dest: "public/files" });
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -11,6 +17,17 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single("upfile"), (req,res) =>{
+  console.log(req.file);
+  if (req.file)
+  {
+    res.json({ name: req.file.originalname, type: req.file.mimetype, size: req.file.size});
+  }
+  else
+  {
+    res.json({ error: "File not found!"})
+  }
+})
 
 
 
